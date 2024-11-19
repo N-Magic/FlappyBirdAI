a05fe4ee376a8d278ba438c4a409d9f55e2eabd4
N-magic <n8martin2025@gmail.com>
2024-11-19 13:00:39 -0600

    created a bird class

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ var ctx = screen.getContext("2d");
 var bird = new Image();
 bird.src = "./bird.png";
 ctx.drawImage(bird, 0, 0, 60, 50);
+
+var gravity = 10;
+var flap_stregth = 10;
+
+class bird {
+  constructor(height, vel) {
+    this.height = height;
+    this.speed = vel;
+  }
+}
